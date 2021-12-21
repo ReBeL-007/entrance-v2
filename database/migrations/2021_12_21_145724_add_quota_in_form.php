f7a6ec6c8584a6ea32dbb0153a5b671a896578b8
ReBeL-007 <rebelprajapati88@gmail.com>
2021-12-21 21:24:46 +0545

    first commit

---
+++ database/migrations/2021_12_21_145724_add_quota_in_form.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddQuotaInForm extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('forms', function (Blueprint $table) {
+            //
+            $table->string('quota')->nullable();
+            $table->string('full_paying_quota')->nullable();
+            $table->string('gov_inclusion_quota')->nullable();
+            $table->string('scholarship_quota')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('forms', function (Blueprint $table) {
+            //
+        });
+    }
+}
